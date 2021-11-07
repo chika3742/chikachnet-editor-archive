9891e83c6b0853bd58b9ec398bf0139716772f9c
chika3742 <kazu.chika.shima@gmail.com>
2021-11-07 15:57:45 +0900

    fix

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -16,9 +16,9 @@ firebase.initializeApp(firebaseConfig)
 try {
   const messaging = firebase.messaging()
 
-  messaging.onBackgroundMessage(function(payload) {
-    self.registration.showNotification(payload.data.title, payload.data)
-  })
+  // messaging.onBackgroundMessage(function(payload) {
+  // self.registration.showNotification(payload.data.title, payload.notification)
+  // })
 } catch (e) {
   console.error(e)
 }
